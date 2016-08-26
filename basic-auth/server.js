43ca0bce2ce7ce7d5585082a637e16d384f63d4c
lastminute84 <csabafarkas17@gmail.com>
2016-08-26 23:25:19 +0100

    Signed cookies

--- basic-auth/server.js
+++ basic-auth/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+
 
 var hostname = 'localhost';
 var port = 3000;
@@ -8,25 +10,46 @@ var app = express();
 
 app.use(morgan('dev'));
 
+//Use cookie-parser as a middleware - give secret key as a parameter
+app.use(cookieParser('12345-67890-09876-54321'));
+
 function auth (req, res, next) {
     console.log(req.headers);
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
-        return;
-    }
 
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        next(); // authorized
+    //Check if request does not contain a 'user' value of the signed cookie.
+    if(!req.signedCookies.user) {
+        var authHeader = req.headers.authorization;
+        if (!authHeader) {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+            return;
+        }
+
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user == 'admin' && pass == 'password') {
+            //If authenticated, create a 'user' value in the signed cookie and sign it with the secret key
+            //Parameters are as follows: res.cookie(name, value, options)
+            res.cookie('user', 'admin', {signed: true});
+            next(); // authorized
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
     } else {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
+        //If the cookie in the request's header contains user value, check the username
+        if(req.signedCookies.user === 'admin') {
+            console.log(req.signedCookies);
+            next();
+        } else {
+            //Validation failed, username is not 'admin'
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
     }
 }
 
